04526b913f4a24b59ec5e027375fa8b346c0c92b
jarikexe <jarikexe1@gmail.com>
2022-06-25 17:22:12 +0300

    asdfasdf

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -4,8 +4,8 @@ import Box from '@mui/material/Box';
 const Page1 = () => {
 
     return (
-        <Box sx={{height: "90vh"}}>
-            <iframe width="100%" height="100%" src="https://mamentor.co.kr/keyword" title='test'></iframe>
+        <Box sx={{maxHeight: "427px", height:'100vh', overflow: "hidden", position: "relative"}}>
+            <iframe scrolling="no" style={{marginTop: "20px", transform: "scale(1.1)", position: "absolute"}} width="100%" height="100%" src="https://mamentor.co.kr/keyword#keywords" title='test'></iframe>
         </Box>
     );
 }
