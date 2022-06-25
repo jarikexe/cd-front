11f4cfae005d9671d05c05b58f2005c555be43da
jarikexe <jarikexe1@gmail.com>
2022-06-25 18:14:16 +0300

    asdfasdf

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,11 +1,27 @@
 import Box from '@mui/material/Box';
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
+import Container from "@mui/material/Container";
 
 
 const Page1 = () => {
 
     return (
-        <Box sx={{maxHeight: "427px", height:'100vh', overflow: "hidden", position: "relative"}}>
-            <iframe scrolling="no" style={{marginTop: "20px", transform: "scale(1.1)", position: "absolute"}} width="100%" height="100%" src="https://mamentor.co.kr/keyword#keywords" title='test'></iframe>
+        <Box sx={{maxHeight: "450px", height: '100vh', overflow: "hidden", position: "relative"}}>
+            {window.innerWidth > 1200 ?
+                <iframe scrolling="no" style={{marginTop: "43px", transform: "scale(1.2)", position: "absolute"}}
+                        width="100%" height="100%" src="https://mamentor.co.kr/keyword#keywords" title='test'></iframe>
+                :
+                <Container component="main" maxWidth="xs" sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                    <ReportGmailerrorredIcon sx={{fontSize: 100, color: 'red'}}/>
+                    <div color="primary">PC버전에서만 가능합니다!</div>
+                </Container>
+            }
         </Box>
     );
 }
--- src/pages/Page2.js
+++ src/pages/Page2.js
@@ -1,13 +1,33 @@
 import Box from '@mui/material/Box';
 import {height} from "@mui/system";
+import ReportGmailerrorredIcon from "@mui/icons-material/ReportGmailerrorred";
+import Container from "@mui/material/Container";
 
 
 const Page2 = () => {
 
     return (
-        <Box sx={{height: "1140px", margin: "0 auto", overflow: "hidden", width: "1200px"}}>
-            <iframe scrolling='no' style={{transform: 'scale(1.14)', marginTop: "-350px", marginRight: "-100px"}} width="100%" height="100%" src="https://mamentor.co.kr/shopping_ranking#container" title='test'></iframe>
-        </Box>
+        window.innerWidth > 1200 ?
+
+            <Box sx={{height: "1140px", margin: "0 auto", overflow: "hidden", width: "1200px"}}>
+                <iframe scrolling='no'
+                        style={{transform: 'scale(1.14)', marginTop: "-350px", marginRight: "-100px"}}
+                        width="100%" height="100%" src="https://mamentor.co.kr/shopping_ranking#container"
+                        title='test'></iframe>
+
+            </Box>
+            :
+
+            <Container component="main" maxWidth="xs" sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}>
+                <ReportGmailerrorredIcon sx={{fontSize: 100, color: 'red'}}/>
+                <div color="primary">PC버전에서만 가능합니다!</div>
+            </Container>
+
     );
 }
 
